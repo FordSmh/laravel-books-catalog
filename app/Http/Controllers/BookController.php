71e5003e98f459099610b4a1235d62a2b9457ed6
fordsmh <vasyukvas@gmail.com>
2022-05-17 23:19:18 +0700

    Update: Add Book model and controller

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Author;
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $books = Book::get();
+        return view('book/index', ['books' => $books]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $authors = Author::get();
+        return view('book/form', compact('authors'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $book = new Book;
+
+        $book->title = $request->title;
+        $book->publish_date = $request->publish_date;
+        $book->description = $request->description;
+
+        $book->save();
+        $book->authors()->attach($request->authors);
+        return redirect()->route('books.index')->withSuccess('Book '.$book->title.' has been added');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Book  $book
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\Response
+     */
+    public function show(Book $book)
+    {
+        return view('book/view', compact('book'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Book  $book
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\Response
+     */
+    public function edit(Book $book)
+    {
+        $authors = Author::get();
+        return view('book/form', compact(['authors', 'book']));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Book $book)
+    {
+        $book->title = $request->title;
+        $book->publish_date = $request->publish_date;
+        $book->description = $request->description;
+
+        $book->authors()->detach($book->authors);
+        $book->authors()->attach($request->authors);
+
+        $book->save();
+        return redirect()->route('books.index')->withSuccess('Book '.$book->name.' has been updated');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Book $book)
+    {
+        $book->books()->detach($book->authors);
+        $book->delete();
+        return redirect()->route('books.index')->withDanger('Book '.$book->title.' has been deleted');
+    }
+}
